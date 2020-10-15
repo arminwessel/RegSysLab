210fa8cf072a07bc260a564e23a1df58ecbd9b99
Nightshadow1258 <paul.reisch43@gmail.com>
2020-10-15 09:25:25 +0200

    Korrektur

--- Lab1/uebung1/UE_1_2.m
+++ Lab1/uebung1/UE_1_2.m
@@ -36,7 +36,7 @@ axis padded;
 
 %PRBS mit Überabtastung
 Pp=4;
-[u_pr4,t_pr4] = PRBS2(U,Pp,Ta);
+[u_pr4,t_pr4] = PRBS(U,Pp,Ta);
 figure(3)
 stairs(t_pr4,u_pr4);
 grid on;
@@ -60,9 +60,9 @@ u_pr1_ampl = 20*log10(abs(u_pr1_fft));
 u_pr1_phase = unwrap(angle(u_pr1_fft))*(180/pi);
 
 % delta omega
-dw_chirp = 2*pi/(N_chirp*Ta);
-dw_prbs4 = 2*pi/(N_prbs4*Ta);
-dw_prbs1 = 2*pi/(N_prbs1*Ta);
+dw_chirp = 2*pi/(length(u_ch)*Ta);
+dw_prbs4 = 2*pi/(length(u_pr4)*Ta);
+dw_prbs1 = 2*pi/(length(u_pr1)*Ta);
 
 % omega max
 w_max = pi/Ta;
@@ -76,11 +76,11 @@ w_prbs1 = 0:dw_prbs1:w_max-dw_prbs1;
 % BODE plot
 figure(4)
 subplot(2,1,1)
-semilogx(w_chirp, u_ch_ampl(1:floor(N_chirp/2)));
+semilogx(w_chirp, u_ch_ampl(1:floor(length(u_ch)/2)));
 hold on;
-semilogx(w_prbs1, u_pr1_ampl(1:floor(N_prbs1/2)));
+semilogx(w_prbs1, u_pr1_ampl(1:floor(length(u_pr1)/2)));
 hold on;
-semilogx(w_prbs4, u_pr4_ampl(1:floor(N_prbs4/2)));
+semilogx(w_prbs4, u_pr4_ampl(1:floor(length(u_pr4)/2)));
 grid on;
 title('Amplitudengang');
 ylabel('Amplitude [dB]');
@@ -89,11 +89,11 @@ xlabel('Frequenz [rad/s]');
 legend('Chirp-Signal', 'PRBS, Pp = 1', 'PRBS, Pp = 4','Location','southwest');
 
 subplot(2,1,2)
-semilogx(w_chirp, u_ch_phase(1:floor(N_chirp/2)));
+semilogx(w_chirp, u_ch_phase(1:floor(length(u_ch)/2)));
 hold on;
-semilogx(w_prbs1, u_pr1_phase(1:floor(N_prbs1/2)));
+semilogx(w_prbs1, u_pr1_phase(1:floor(length(u_pr1)/2)));
 hold on;
-semilogx(w_prbs4, u_pr4_phase(1:floor(N_prbs4/2)));
+semilogx(w_prbs4, u_pr4_phase(1:floor(length(u_pr4)/2)));
 hold off;
 grid on;
 title('Phasengang');
---
+++ Lab1/uebung1/UE_1_3.m
@@ -0,0 +1,25 @@
+%%RegSys1 Ex1
+
+%%Nicht-parametrische Identifikation
+%%
+%1.3
+% ETFE (empirical transfer function estimate)
+%Werte
+U=10;
+Ta=0.025;
+Pp=4;
+Op=10;(N-1)
+N=4096;
+ws=2*pi;
+we=20*pi;
+
+
+
+
+
+
+%Simulationswerte
+t_a=(N-1)*Ta;
+parSim.t_in=t_in;
+%parSim.u_in(N-1)
+
