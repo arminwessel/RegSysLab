7778ac812cee2e39a462c1247ed1c9cba3ab3c44
Nightshadow1258 <paul.reisch43@gmail.com>
2020-10-27 09:08:58 +0100

    Linearisierung Maple fertig

---
+++ Lab2/uebung_2/matlab/DP_System.m
@@ -0,0 +1,23 @@
+function function [sysk,sysd] = DP_System(parDP)
+%
+%
+
+%% Parameter
+   
+l1=parDP.l1
+l2=parDP.l2
+m1=parDP.m1
+m2=parDP.m2
+a1=parDP.a1
+a2=parDP.a2
+J1=parDP.J1
+J2=parDP.J2
+d1=parDP.d1
+d2=parDP.d2
+m=parDP.m
+g=parDP.g
+
+
+
+end
+
---
+++ Lab2/uebung_2/matlab/Parameter_Liste.m
@@ -0,0 +1,14 @@
+%% Parameter
+
+parDP.l1=0.419; %Laenge inneres Pendel
+parDP.l2=0.484; %Laenge aeußeres Pendel
+parDP.m1=0.9363; %Masse inneres Pendel
+parDP.m2=0.5505; %Masse aeußeres Pendel
+parDP.a1=0.2687; %Abstand Aufhaengung zu Schwerpunkt inneres Pendel
+parDP.a2=0.2256; %Abstand Aufhaengung zu Schwerpunkt aeußeres Pendel
+parDP.J1=0.0243; %Traegheitsmoment inneres Pendel
+parDP.J2=0.0183; %Traegheitsmoment aeußeres Pendel
+parDP.d1=0.0042; %Daempfungskonstante inneres Pendel
+parDP.d2=0.0003; %Daempfungskonstante aeußeres Pendel
+parDP.m=0.2;     %Masse Wagen [m]
+parDP.g=9.81;   %Gravitationskonstante
