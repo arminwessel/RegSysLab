7abc5c9fd8ef2d6df8cd53bdd8b6f5ad8d6922c8
Nightshadow1258 <paul.reisch43@gmail.com>
2020-11-26 12:06:47 +0100

    Matlab file vom ACIN hinzugefügt und in den Folder 2Tank Modell ebenfalls eingefügt für Aufgabe 4.4

---
+++ Lab4/2-Tank-System/set_parameter.m
@@ -0,0 +1,47 @@
+% Parameterdatei Dreitank
+% Uebung Regelungssysteme
+%
+% Erstellt:  19.10.2010 T. Utz
+% Geaendert: 14.7.2011 F. Koenigseder
+%            06.11.2014 S. Flixeder
+% --------------------------------------
+%
+
+clc
+close all;
+clear;
+
+% Parameter des Systems
+parSys.Atank     = 1.539e-2;      % Grundfläche Tank
+parSys.rho       = 998;           % Dichte Wasser
+parSys.eta       = 8.9e-4;        % Dynamische Viskosität Wasser
+parSys.g         = 9.81;          % Gravitationskonstante
+parSys.alphaA1   = 0.0583;        % Kontraktionskoeffizient AV1
+parSys.DA1       = 15e-3;         % Durchmesser AV1
+parSys.alphaA2   = 0.1039;        % Kontraktionskoeffizient AV2
+parSys.A2        = 1.0429e-4;     % Querschnittsflaeche AV2
+parSys.alphaA3   = 0.0600;        % Kontraktionskoeffizient AV3
+parSys.DA3       = 15e-3;         % Durchmesser AV3
+parSys.alpha12_0 = 0.3038;        % Kontraktionskoeffizient ZV12
+parSys.A12       = 5.5531e-5;     % Querschnittsflaeche ZV12
+parSys.Dh12      = 7.7e-3;        % hydraulischer Durchmesser
+parSys.lambdac12 = 24000;         % kritische Fliesszahl ZV12
+parSys.alpha23_0 = 0.1344;        % Kontraktionskoeffizient ZV23
+parSys.D23       = 15e-3;         % Durchmesser ZV23
+parSys.lambdac23 = 29600;         % Kritische Fliesszahl ZV23
+parSys.hmin      = 0.05;          % Minimale Fuellhoehe
+parSys.hmax      = 0.55;          % Maximale Fuellhoehe
+
+% Abtastzeit
+parSys.Ta = 0.2;                
+
+% Anfangsbedingung
+parSys.h1_0 = 0.30;
+parSys.h2_0 = 0.10;
+parSys.h3_0 = 0.05;
+
+% Maximale Zufluesse
+parSys.qZ1max = 4.5e-3/60;        % Maximaler Zufluss Z1
+parSys.qZ3max = 4.5e-3/60;        % Maximaler Zufluss Z3
+parSys.qZ1min = 0;                % Minimaler Zufluss Z1
+parSys.qZ3min = 0;                % Minimaler Zufluss Z3
